cbf88e50df20957ffba95b5c9e325182512b9343
zeze1004 <thwjd2717@naver.com>
2020-08-03 20:12:21 +0900

    0803 스터디 시작

--- ZeZe/GFstudy/Index.js
+++ ZeZe/GFstudy/Index.js
@@ -60,7 +60,7 @@ app.get('/message',function(req,res){
 
 
 
-//app.use(express.static('views.silverwo.png')); -> 은우사진 띄우기
+//app.use(express.static('views.silverwo.png')); 
 
 //DB연결 json 방식
 mongoose.connect('mongodb+srv://admin:cafe9192@mycluster.jezsv.gcp.mongodb.net/MyCluster?retryWrites=true&w=majority',{
--- ZeZe/GFstudy/models/Message.js
+++ ZeZe/GFstudy/models/Message.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema(
     {
-        content: String
+        content: String,
+        //author: String
+        author: {   
+            type: String,
+            unique: true    //중복된 값에 들어갈 수 없음
+        }
     });
     // 이게 모지?
     module.exports = mongoose.model('Message',MessageSchema);
